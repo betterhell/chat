c91c8ca83f2fea5f8944aaa67e32bbed28fb9cf3
betterhell <dev.betterhell@gmail.com>
2023-05-30 13:30:00 +0300

    starting create user controllers and routes

--- server/index.ts
+++ server/index.ts
@@ -1,111 +1,35 @@
-import {Request, Response} from "express";
-import {ObjectId} from "mongodb";
-import {handleError} from "./handlers/handleError";
-import {handleResponse} from "./handlers/handleResponse";
-
 const PORT = 5000
+const DB_URL = 'mongodb+srv://betterhell:598213q@chatcluster.olsky05.mongodb.net/chatDB?retryWrites=true&w=majority';
 const cors = require("cors")
 const express = require("express")
-
+const mongoose = require("mongoose")
 const app = express()
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 const httpServer = require("http").Server(app)
-const {connectToDb, getDb} = require("./db")
-
-app.use(cors());
-app.use(express.json())
+const movieRouter = require("./routes/movie.routes")
+const userRouter = require("./routes/user.routes")
 
 export let users = []
 
-let db
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(movieRouter)
+app.use(userRouter)
 
-connectToDb((error) => {
-    if (!error) {
-        httpServer.listen(PORT, (err) => {
-            err ? console.log(err) : console.log(`Server is up on port ${PORT}!`)
-        })
-        db = getDb()
-    } else {
-        console.log(`DB connection error: ${error}`)
-    }
+mongoose
+    .connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.log(`DB connection error: ${error}`))
+
+httpServer.listen(PORT, (err) => {
+    err ? console.log(err) : console.log(`Server is up on port ${PORT}!`)
 })
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello API!')
-})
-
-// get all movies
-app.get('/movies', (req: Request, res: Response) => {
-    const movies = []
-
-    db
-        .collection("movies")
-        .find()
-        .forEach((movie) => movies.push(movie))
-        .then(() => handleResponse(res, 200, movies))
-        .catch(() => handleError(res, "Something went wrong... Try later."))
-})
-
-// get one movie
-app.get('/movies/:id', (req: Request, res: Response) => {
-    const movieId = req.params.id
-
-    if (ObjectId.isValid(movieId)) {
-        db
-            .collection("movies")
-            .findOne({_id: new ObjectId(movieId)})
-            .then((document) => handleResponse(res, 200, document))
-            .catch(() => handleError(res, "Something went wrong... Try later."))
-    } else {
-        handleError(res, "ID is not valid!")
-    }
-})
-
-// delete movie
-app.delete('/movies/:id', (req: Request, res: Response) => {
-    const movieId = req.params.id
-
-    if (ObjectId.isValid(movieId)) {
-        db
-            .collection("movies")
-            .deleteOne({_id: new ObjectId(movieId)})
-            .then(() => handleResponse(res, 200, "Movie successful delete!"))
-            .catch(() => handleError(res, "Something went wrong... Try later."))
-    } else {
-        handleError(res, "ID is not valid!")
-    }
-})
-
-
-// post new movie
-app.post('/movies', (req: Request, res: Response) => {
-    const newMovie = req.body
-
-    db
-        .collection("movies")
-        .insertOne(newMovie)
-        .then((result) => handleResponse(res, 201, `New movie with id = ${result.insertedId} added!`))
-        .catch(() => handleError(res, "Something went wrong... Try later."))
-})
-
-// update movie
-app.patch('/movies/:id', (req: Request, res: Response) => {
-    const movieId = req.params.id
-    const updatedMovieInfo = req.body
-
-    if (ObjectId.isValid(movieId)) {
-        db
-            .collection("movies")
-            .updateOne({_id: new ObjectId(movieId)}, {$set: updatedMovieInfo})
-            .then(() => handleResponse(res, 200, `Movie successful with id ${movieId} updated!`))
-            .catch(() => handleError(res, "Something went wrong... Try later."))
-    } else {
-        handleError(res, "ID is not valid!")
-    }
-})
-
 
 //socket.io connection
-const io = require("socket.io")(httpServer, {
+export const io = require("socket.io")(httpServer, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
@@ -135,4 +59,5 @@ io.on("connection", (client) => {
         io.emit('responseEndTyping', data)
     })
 })
+
 
