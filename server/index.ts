dcd25cd3520d0e2cd8515b24af79d5628e9dcbc4
betterhell <dev.betterhell@gmail.com>
2023-05-30 08:46:23 +0300

    finish CRUD examples

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,7 @@
 import {Request, Response} from "express";
+import {ObjectId} from "mongodb";
+import {handleError} from "./handlers/handleError";
+import {handleResponse} from "./handlers/handleResponse";
 
 const PORT = 5000
 const cors = require("cors")
@@ -9,6 +12,7 @@ const httpServer = require("http").Server(app)
 const {connectToDb, getDb} = require("./db")
 
 app.use(cors());
+app.use(express.json())
 
 export let users = []
 
@@ -26,10 +30,10 @@ connectToDb((error) => {
 })
 
 app.get('/', (req: Request, res: Response) => {
-
     res.send('Hello API!')
 })
 
+// get all movies
 app.get('/movies', (req: Request, res: Response) => {
     const movies = []
 
@@ -37,17 +41,68 @@ app.get('/movies', (req: Request, res: Response) => {
         .collection("movies")
         .find()
         .forEach((movie) => movies.push(movie))
-        .then(() => {
-            res
-                .status(200)
-                .json(movies)
-        })
-        .catch(() => {
-            res
-                .status(500)
-                .json({error: "Something went wrong... Try later."})
-        })
+        .then(() => handleResponse(res, 200, movies))
+        .catch(() => handleError(res, "Something went wrong... Try later."))
 })
+
+// get one movie
+app.get('/movies/:id', (req: Request, res: Response) => {
+    const movieId = req.params.id
+
+    if (ObjectId.isValid(movieId)) {
+        db
+            .collection("movies")
+            .findOne({_id: new ObjectId(movieId)})
+            .then((document) => handleResponse(res, 200, document))
+            .catch(() => handleError(res, "Something went wrong... Try later."))
+    } else {
+        handleError(res, "ID is not valid!")
+    }
+})
+
+// delete movie
+app.delete('/movies/:id', (req: Request, res: Response) => {
+    const movieId = req.params.id
+
+    if (ObjectId.isValid(movieId)) {
+        db
+            .collection("movies")
+            .deleteOne({_id: new ObjectId(movieId)})
+            .then(() => handleResponse(res, 200, "Movie successful delete!"))
+            .catch(() => handleError(res, "Something went wrong... Try later."))
+    } else {
+        handleError(res, "ID is not valid!")
+    }
+})
+
+
+// post new movie
+app.post('/movies', (req: Request, res: Response) => {
+    const newMovie = req.body
+
+    db
+        .collection("movies")
+        .insertOne(newMovie)
+        .then((result) => handleResponse(res, 201, `New movie with id = ${result.insertedId} added!`))
+        .catch(() => handleError(res, "Something went wrong... Try later."))
+})
+
+// update movie
+app.patch('/movies/:id', (req: Request, res: Response) => {
+    const movieId = req.params.id
+    const updatedMovieInfo = req.body
+
+    if (ObjectId.isValid(movieId)) {
+        db
+            .collection("movies")
+            .updateOne({_id: new ObjectId(movieId)}, {$set: updatedMovieInfo})
+            .then(() => handleResponse(res, 200, `Movie successful with id ${movieId} updated!`))
+            .catch(() => handleError(res, "Something went wrong... Try later."))
+    } else {
+        handleError(res, "ID is not valid!")
+    }
+})
+
 
 //socket.io connection
 const io = require("socket.io")(httpServer, {
