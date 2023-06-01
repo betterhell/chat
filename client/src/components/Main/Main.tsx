d73c70ff153471f8aabc65806432d34e914dea67
betterhell <dev.betterhell@gmail.com>
2023-06-01 17:57:43 +0300

    rework controllers, create services and more

---
+++ client/src/components/Main/Main.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import styles from "./styles.module.scss"
+
+const Main = () => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.context}>
+                <h1><Link to="/sign-in">Welcome to the CHAT!</Link></h1>
+            </div>
+
+            <div className={styles.area}>
+                <ul className={styles.circles}>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
