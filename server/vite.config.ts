e7a34e1d5cd537f7e7dc309d75e0650d8289cfc8
betterhell <skkogame@gmail.com>
2025-07-12 23:47:36 +0300

    Reinstall

--- server/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import {defineConfig} from 'vite'
-import EnvironmentPlugin from "vite-plugin-environment";
-
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [EnvironmentPlugin('all')],
-})
